3abadc67ddf459fc8279e72c5e800a0e5ce913e5
enesusta <enesusta@email.com>
2021-01-02 16:17:06 +0300

    Initial commit

---
+++ src/main/java/com/github/enesusta/App.java
@@ -0,0 +1,12 @@
+package com.github.enesusta;
+
+import java.io.IOException;
+
+public final class App {
+
+    public static void main(String[] args) throws IOException {
+        User user = new User("enes", "usta");
+        System.out.println(user);
+    }
+
+}
